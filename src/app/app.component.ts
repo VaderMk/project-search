d15630c585cf9fb98861fafdf329986abf6ef56f
VaderMk <112209420+VaderMk@users.noreply.github.com>
2023-01-27 14:19:12 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'instant-search';
+  public searchInput!: string;
+  public programmingLanguages = ['Python','TypeScript','C','C++','Java',
+  'Go','JavaScript','PHP','Ruby','Swift','Kotlin']
+}
